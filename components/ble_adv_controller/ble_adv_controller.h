aa73c32dc040f62cde76dfac0e84b4ba2f08d4d0
HookiBeers <161614127+HookiBeers@users.noreply.github.com>
2025-08-22 11:13:56 +0200

    Update ble_adv_controller.h

--- components/ble_adv_controller/ble_adv_controller.h
+++ components/ble_adv_controller/ble_adv_controller.h
@@ -43,7 +43,7 @@ protected:
 /**
   BleAdvSelect: basic implementation of 'Select' to handle configuration choice from HA directly
  */
-class BleAdvSelect: public BleAdvDynConfig < select::Select > {
+class BleAdvSelect: public BleAdvDynConfig < select::Select >, public Component {
 protected:
   void control(const std::string &value) override;
   void sub_init() override;
@@ -52,7 +52,7 @@ protected:
 /**
   BleAdvNumber: basic implementation of 'Number' to handle duration(s) choice from HA directly
  */
-class BleAdvNumber: public BleAdvDynConfig < number::Number > {
+class BleAdvNumber: public BleAdvDynConfig < number::Number >, public Component {
 protected:
   void control(float value) override;
   void sub_init() override;
